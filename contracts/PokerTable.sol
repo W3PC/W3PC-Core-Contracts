2102cf6c2f6b007b62cece44a82f1723a2f52f9b
fullyallocated <fullyallocated@gmail.com>
2022-03-30 00:48:24 -0400

    barebonez

---
+++ contracts/PokerTable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0
+
+pragma solidity ^0.8.10;
+
+import "@rari-capital/solmate/src/tokens/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PokerTable is Ownable {  
+  ERC20 public immutable pokerDaoChips;
+
+  constructor(ERC20 chipToken_) {
+    pokerDaoChips = chipToken_;
+  }
+
+  function sitIn(uint256 amount_) external {
+    pokerDaoChips.transferFrom(msg.sender, address(this), amount_);
+  }
+
+  function leaveTable(address player_, uint256 amount_) external onlyOwner {
+    pokerDaoChips.transfer(player_, amount_);
+  }
+}
